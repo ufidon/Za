ce5692f9fa578c1440c0afbceaf10ed8e826478d
ufidon <ufidon@gmail.com>
2022-10-09 14:26:26 -0400

    录屏cpu加速

--- tools/record.bat
+++ tools/record.bat
@@ -3,4 +3,8 @@ REM https://trac.ffmpeg.org/wiki/ChangingFrameRate
 REM ffmpeg -i <input> -filter:v fps=30 <output>
 REM 远程录屏
 
-ffmpeg  -f dshow -i audio="Remote Audio"  -f gdigrab -framerate 10  -i desktop -c:v h264_nvenc -qp 0  %1 
+REM GPU加速
+REM ffmpeg  -f dshow -i audio="Remote Audio"  -f gdigrab -framerate 10  -i desktop -c:v h264_nvenc -qp 0  %1 
+
+REM CPU加速
+ffmpeg  -f dshow -i audio="Remote Audio"  -f gdigrab -framerate 10  -i desktop -c:v h264_qsv -qp 0  %1
